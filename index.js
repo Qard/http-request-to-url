ee4b639701c065629bf8d65e26835e2141d81616
Alan Storm <astorm@alanstorm.com>
2021-04-02 09:54:01 -0700

    fix: follow proxies

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const awaitEvent = require('await-event')
 const socketLocation = require('socket-location')
 
-module.exports = async function httpRequestToUrl (request) {
+module.exports = async function httpRequestToUrl (request, opts = {}) {
+  // default to true
+  opts.followProxies = opts.hasOwnProperty('followProxies') ? opts.followProxies : true
+
   if (!request.socket) {
     await awaitEvent(request, 'socket')
   }
@@ -10,5 +13,14 @@ module.exports = async function httpRequestToUrl (request) {
   const proto = `http${socket.encrypted ? 's' : ''}:`
   const location = await socketLocation(socket)
 
+  if (isProxiedRequest(request) && opts.followProxies) {
+    return request.path
+  }
+
   return `${proto}//${location}${request.path}`
+}
+
+function isProxiedRequest (request) {
+  console.log(request.path)
+  return request.path.indexOf('https:') === 0 || request.path.indexOf('http:') === 0
 }
