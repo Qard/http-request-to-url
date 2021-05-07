7406c37baad7625c1bdfa17c1b62b821f55fbe03
Alan Storm <astorm@users.noreply.github.com>
2021-05-06 19:25:29 -0700

    feat: follow proxies (#2)

--- test.js
+++ test.js
@@ -29,6 +29,44 @@ tap.test('after connect', async t => {
   t.end()
 })
 
+tap.test('proxy', async t => {
+  const { port } = await makeServer()
+
+  const opts = {
+    host: '127.0.0.1',
+    port,
+    method: 'GET',
+    path: 'http://example.com/foo'
+  }
+  const client = http.request(opts, res => res.resume())
+  client.end()
+
+  const location = await httpRequestToUrl(client)
+  t.equal(location, 'http://example.com/foo')
+
+  const location2 = await httpRequestToUrl(client, { followProxies: true })
+  t.equal(location2, 'http://example.com/foo')
+
+  t.end()
+})
+
+tap.test('proxy do not follow', async t => {
+  const { port } = await makeServer()
+
+  const opts = {
+    host: '127.0.0.1',
+    port,
+    method: 'GET',
+    path: 'http://example.com/foo'
+  }
+  const client = http.request(opts, res => res.resume())
+  client.end()
+
+  const location = await httpRequestToUrl(client, { followProxies: false })
+  t.equal(location, `http://127.0.0.1:${port}`)
+
+  t.end()
+})
 function makeServer () {
   return new Promise(resolve => {
     const server = http.createServer((req, res) => {
